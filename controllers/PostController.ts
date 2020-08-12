b9b66a3a9a75123680f462755a737be331bdafd2
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-08-12 08:36:55 +0400

    Create findPost method in BaseController

--- controllers/PostController.ts
+++ controllers/PostController.ts
@@ -1,17 +1,45 @@
 import BaseController from "./BaseController.ts";
-import {RouterContext} from '../deps.ts';
-import { queryOne } from "../database.ts";
+import { RouterContext } from "../deps.ts";
+import { queryOne, queryArray } from "../database.ts";
 
 class PostController extends BaseController {
+  async getAll(ctx: RouterContext) {
+    const user = ctx.state.user;
+
+    try {
+      let posts = [];
+      if (user) {
+        posts = await queryArray(
+          `SELECT p.* FROM posts p 
+          LEFT JOIN subreddits s on s.id = p.subreddit_id
+          LEFT JOIN subreddits_users su ON su.subreddit_id = s.id
+          WHERE su.user_id = $1 
+          ORDER BY  p.create_date DESC`,
+          user.id,
+        );
+      } else {
+        posts = await queryArray(`SELECT p.* FROM posts p
+        ORDER BY p.upvotes`);
+      }
+      return this.response(ctx, posts);
+    } catch (e) {
+      console.log(e);
+      return this.internalServerError(ctx);
+    }
+  }
+
   async create(ctx: RouterContext) {
     const subredditId = ctx.params.id;
     const body = await ctx.request.body();
     const data = await body.value;
     const user = ctx.state.user;
     try {
-      let subreddit = await queryOne(`SELECT * FROM subreddits WHERE id = $1`, subredditId);
+      let subreddit = await queryOne(
+        `SELECT * FROM subreddits WHERE id = $1`,
+        subredditId,
+      );
       if (!subreddit) {
-        return this.response(ctx, {message: `Subreddit does not exist`}, 404);
+        return this.response(ctx, { message: `Subreddit does not exist` }, 404);
       }
 
       let post = {
@@ -23,18 +51,19 @@ class PostController extends BaseController {
         user_id: user.id,
         create_date: new Date(),
         upvotes: 1,
-        type: data.type
+        type: data.type,
       };
 
-      post = await queryOne(`INSERT INTO posts 
+      post = await queryOne(
+        `INSERT INTO posts 
             (subreddit_id, title, text, image_video, link, user_id, create_date, upvotes, type)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
-            ...Object.values(post)
-      )
+        ...Object.values(post),
+      );
       return this.response(ctx, post, 201);
-    } catch(e) {
+    } catch (e) {
       console.log(e);
-      return this.response(ctx, {message: 'Internal Server error'}, 500);
+      return this.internalServerError(ctx);
     }
   }
 
@@ -45,23 +74,37 @@ class PostController extends BaseController {
     const user = ctx.state.user;
 
     try {
-      let post = await queryOne(`SELECT * FROM posts WHERE id = $1`, id);
+      let post = await this.findPost(ctx, id, user.id);
       if (!post) {
-        return this.response(ctx, {message: `Subreddit does not exist`}, 404);
+        return;
       }
-      if (post.user_id !== user.id) {
-        return this.response(ctx, {message: `You don't have permission to update this post`}, 403);
-      }
-
-      post = await queryOne(`UPDATE posts SET text = $1 WHERE id = $2 RETURNING *`,
+      await queryOne(
+        `UPDATE posts SET text = $1 WHERE id = $2 RETURNING *`,
         data.text,
-        id
-      )
+        id,
+      );
+      post.text = data.text;
+      console.log(post);
       return this.response(ctx, post, 200);
-
-    } catch(e) {
+    } catch (e) {
       console.log(e);
-      return this.response(ctx, {message: "Internal server error"}, 500);
+      return this.internalServerError(ctx);
+    }
+  }
+
+  async delete(ctx: RouterContext) {
+    const id = ctx.params.id;
+    const user = ctx.state.user;
+    let post = await this.findPost(ctx, id, user.id);
+    if (!post) {
+      return;
+    }
+
+    try {
+      await queryOne(`DELETE FROM posts WHERE id = $1`, id);
+    } catch (e) {
+      console.log(e);
+      return this.internalServerError(ctx);
     }
   }
 }
