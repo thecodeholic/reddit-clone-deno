3ed805414f60b520ab9e0966ff69c1a06364c521
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-09-06 14:59:31 +0400

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -3,9 +3,7 @@ export {
   Router,
   Context,
   RouterContext,
-} from "https://deno.land/x/oak@v6.0.1/mod.ts";
-export { connect } from "https://deno.land/x/cotton@v0.6.3/mod.ts";
-export { Model, Column } from "https://deno.land/x/cotton@v0.6.3/mod.ts";
+} from "https://deno.land/x/oak@v6.1.0/mod.ts";
 export {
   Client,
 } from "https://deno.land/x/postgres@v0.4.3/mod.ts";
@@ -28,4 +26,4 @@ export {
 } from "https://deno.land/x/djwt@v1.2/validate.ts";
 export {
   oakCors,
-} from "https://deno.land/x/cors@v1.1.0/mod.ts";
+} from "https://deno.land/x/cors@v1.2.0/mod.ts";
